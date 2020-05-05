fe20756d9f31d2e3d430ce8335fbdbef3726d6ff
sgire421 <sophie.g.gire@gmail.com>
2020-05-05 13:37:32 -0500

    working on fortune cookie, home buttons created

--- app/src/main/java/com/example/need2hear/FortuneActivity.java
+++ app/src/main/java/com/example/need2hear/FortuneActivity.java
@@ -2,7 +2,10 @@ package com.example.need2hear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FortuneActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class FortuneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fortune);
+        Button home = findViewById(R.id.homeBtn2);
+        home.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToMainActivity();
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
